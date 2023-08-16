874ae02c8c88f05f33fb2e149f4c68c43a628238
Kulikov Vladislav <vladku9867@gmail.com>
2023-08-16 17:42:52 +0300

    getToken function added

--- scripts/game.js
+++ scripts/game.js
@@ -17,6 +17,35 @@ function getComputerChoice() {
     return tokens[getRandomInt(0, tokens.length)];
 }
 
-for (let i = 0; i < 5; i++) {
-    console.log(getComputerChoice());
+function getToken(stringRepresentation) {
+    const string = stringRepresentation.toLowerCase();
+    switch (string) {
+        case "rock":
+            return GameToken.Rock;
+        case "paper":
+            return GameToken.Paper;
+        case "scissors":
+            return GameToken.Scissors;
+        default:
+            console.log("Wrong game token!");
+    }
 }
+
+console.log(getToken("ROCK"));
+console.log(getToken("Rock"));
+console.log(getToken("rock"));
+console.log(getToken("paper"));
+console.log(getToken("scissors"));
+getToken("wrongToken");
+
+function beats(token) {
+    switch (token) {}
+}
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
